ce03d83d5f9e03e968b43dc97ae221804b7e8291
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2020-05-25 21:03:07 +0100

    Arreglando fallos para que funcione en Ghpages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/dsi-p6-win311-alu0100944723/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/dsi-p6-win311-alu0100944723/' : '/'
 }
